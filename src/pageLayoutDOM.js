b78d3223f9706b56b7eaff13a44907a1a285d90f
Callum <callumacourtt@gmail.com>
2023-03-18 18:57:37 +0000

    Finished initial project swithcing and task adding functionality

--- src/pageLayoutDOM.js
+++ src/pageLayoutDOM.js
@@ -1,53 +1,92 @@
 import projectButtonCreator from './projectButtonCreator';
-import { projects } from './projects';
+import { addTaskToProject, projects } from './projects';
+import TaskCreator from './tasks';
+
+function createHeader() {
+  const headerContainer = document.createElement('div');
+  headerContainer.classList.add('headerContainer');
+  document.body.appendChild(headerContainer);
+
+  const header = document.createElement('h2');
+  header.classList.add('header');
+  header.innerText = 'Todo List';
+  headerContainer.appendChild(header);
+}
+
+function createMain() {
+  const main = document.createElement('div');
+  main.classList.add('main');
+  document.body.appendChild(main);
+
+  const projectContainer = document.createElement('div');
+  projectContainer.classList.add('projectsContainer');
+  main.appendChild(projectContainer);
+
+  const tasksContainer = document.createElement('div');
+  tasksContainer.classList.add('tasksContainer');
+  main.appendChild(tasksContainer);
+
+  projectButtonCreator(
+    projects,
+    projectContainer,
+    tasksContainer,
+    createAddTaskButton
+  );
+}
+
+function createFooter() {
+  const footerContainer = document.createElement('div');
+  footerContainer.classList.add('footerContainer');
+  document.body.appendChild(footerContainer);
+
+  const footer = document.createElement('footer');
+  footer.classList.add('footer');
+  footerContainer.appendChild(footer);
+}
+
+export function createAddTaskButton(project, tasksContainer) {
+  const addTaskButton = document.createElement('button');
+  addTaskButton.innerText = '+ Add a task';
+  addTaskButton.classList.add('taskAdderBtn');
+  tasksContainer.appendChild(addTaskButton);
+
+  addTaskButton.addEventListener('click', () => {
+    createTaskInputControls(project, tasksContainer);
+  });
+}
+function createTaskInputControls(project, tasksContainer) {
+  const taskNameInput = document.createElement('input');
+  tasksContainer.appendChild(taskNameInput);
+
+  const confirmInput = document.createElement('button');
+  confirmInput.innerText = '✓';
+  tasksContainer.appendChild(confirmInput);
+
+  const cancelInput = document.createElement('button');
+  cancelInput.innerText = 'X';
+  tasksContainer.appendChild(cancelInput);
+
+  confirmInput.addEventListener('click', () => {
+    console.log(project);
+    const taskName = taskNameInput.value;
+    const newTask = new TaskCreator(taskName, 'h', 'h', 'h');
+    addTaskToProject(project.name, newTask); // Use the project instance directly
+    renderTask(newTask, tasksContainer); // Render the new task immediately
+  });
+
+  cancelInput.addEventListener('click', () => {
+    taskNameInput.value = '';
+  });
+}
+
+function renderTask(task, container) {
+  const taskElement = document.createElement('div');
+  taskElement.textContent = task.title;
+  container.appendChild(taskElement);
+}
 
 export default function createPageLayout() {
-  const createHeader = () => {
-    const HeaderContainer = document.createElement('div');
-    HeaderContainer.classList.add('headerContainer');
-    document.body.appendChild(HeaderContainer);
-
-    const header = document.createElement('h2');
-    header.classList.add('header');
-    header.innerText = 'Todo List';
-    HeaderContainer.appendChild(header);
-  };
-
   createHeader();
-
-  // main.js
-
-  const createMain = () => {
-    const main = document.createElement('div');
-    main.classList.add('main');
-    document.body.appendChild(main);
-
-    const createMainContent = () => {
-      const projectContainer = document.createElement('div');
-      projectContainer.classList.add('projectsContainer');
-      main.appendChild(projectContainer);
-
-      const tasksContainer = document.createElement('div');
-      tasksContainer.classList.add('tasksContainer');
-      main.appendChild(tasksContainer);
-
-      projectButtonCreator(projects, projectContainer, tasksContainer);
-    };
-
-    createMainContent();
-  };
-
   createMain();
-
-  const createFooter = () => {
-    const footerContainer = document.createElement('div');
-    footerContainer.classList.add('footerContainer');
-    document.body.appendChild(footerContainer);
-
-    const footer = document.createElement('footer');
-    footer.classList.add('footer');
-    footerContainer.appendChild(footer);
-  };
-
   createFooter();
 }
--- src/projectButtonCreator.js
+++ src/projectButtonCreator.js
@@ -3,7 +3,8 @@ let selectedProject = null;
 export default function projectButtonCreator(
   projects,
   projectContainer,
-  tasksContainer
+  tasksContainer,
+  createAddTaskButton
 ) {
   projects.forEach(project => {
     const projectButton = document.createElement('button');
@@ -12,14 +13,17 @@ export default function projectButtonCreator(
     projectContainer.appendChild(projectButton);
 
     projectButton.addEventListener('click', () => {
+      tasksContainer.innerHTML = ''; // Clear previous tasks and input controls
       selectedProject = project;
       console.log(selectedProject);
-      tasksContainer.innerText = ''; // clear previous tasks
-      selectedProject.tasks.forEach(task => {
+
+      project.tasks.forEach(task => {
         const taskElement = document.createElement('div');
         taskElement.textContent = task.title;
         tasksContainer.appendChild(taskElement);
       });
+
+      createAddTaskButton(project, tasksContainer);
     });
   });
 }
