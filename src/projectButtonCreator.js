e32838befdbe4baab21f52158272aa760694c3b4
Callum <callumacourtt@gmail.com>
2023-03-18 19:00:14 +0000

    Made code more readable

--- src/projectButtonCreator.js
+++ src/projectButtonCreator.js
@@ -1,4 +1,37 @@
-let selectedProject = null;
+const selectedProject = null;
+
+function createProjectButton(project) {
+  const projectButton = document.createElement('button');
+  projectButton.textContent = project.name;
+  projectButton.classList.add('projectButton');
+
+  return projectButton;
+}
+
+function clearTasksContainer(tasksContainer) {
+  tasksContainer.innerHTML = '';
+}
+
+function renderTasks(project, tasksContainer) {
+  project.tasks.forEach(task => {
+    const taskElement = document.createElement('div');
+    taskElement.textContent = task.title;
+    tasksContainer.appendChild(taskElement);
+  });
+}
+
+function setupProjectButtonEventListener(
+  projectButton,
+  project,
+  tasksContainer,
+  createAddTaskButton
+) {
+  projectButton.addEventListener('click', () => {
+    clearTasksContainer(tasksContainer);
+    renderTasks(project, tasksContainer);
+    createAddTaskButton(project, tasksContainer);
+  });
+}
 
 export default function projectButtonCreator(
   projects,
@@ -7,23 +40,14 @@ export default function projectButtonCreator(
   createAddTaskButton
 ) {
   projects.forEach(project => {
-    const projectButton = document.createElement('button');
-    projectButton.textContent = project.name;
-    projectButton.classList.add('projectButton');
+    const projectButton = createProjectButton(project);
     projectContainer.appendChild(projectButton);
 
-    projectButton.addEventListener('click', () => {
-      tasksContainer.innerHTML = ''; // Clear previous tasks and input controls
-      selectedProject = project;
-      console.log(selectedProject);
-
-      project.tasks.forEach(task => {
-        const taskElement = document.createElement('div');
-        taskElement.textContent = task.title;
-        tasksContainer.appendChild(taskElement);
-      });
-
-      createAddTaskButton(project, tasksContainer);
-    });
+    setupProjectButtonEventListener(
+      projectButton,
+      project,
+      tasksContainer,
+      createAddTaskButton
+    );
   });
 }
